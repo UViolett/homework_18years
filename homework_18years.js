3de0d00f17de3123c89e10b0c237ac464030e5c6
Violetta Usanova <usanovavm@gmail.com>
2017-07-15 14:13:48 +0300

    added file homework_18years.js

---
+++ homework_18years.js
@@ -0,0 +1,5 @@
+'use strict'
+
+var age = +prompt('Введите значение'); //will return a number 
+
+0 < age && age >= 18 ? alert('Вы входите в закрытый раздел сайта') : alert('Возраст введен некорректно'); 
